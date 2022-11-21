7da5d19a015912f6083c5a00087269b47c5757c7
NagaitsevEP <nagaitsev@outlook.com>
2022-11-21 13:08:57 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Eugene Nagaitsev");
+        System.out.println("27.01.1983");
+    }
+}
