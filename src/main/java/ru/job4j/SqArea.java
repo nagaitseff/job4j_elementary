8869b502f3e6933a74654e7328292e5b929942a8
Нагайцев Евгений Петрович <nagaitsev@outlook.com>
2022-11-25 10:01:16 +0700

    8. Стороны прямоугольника. [#137735 #363383]

--- src/main/java/ru/job4j/SqArea.java
+++ src/main/java/ru/job4j/SqArea.java
@@ -4,8 +4,7 @@ public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double s = l * h;
-        double rsl = s;
+        double rsl = l * h;
         return rsl;
     }
 
