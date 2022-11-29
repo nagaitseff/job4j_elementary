1c3bec1d3b56118d53e650c35ac038474e12bdef
Нагайцев Евгений Петрович <nagaitsev@outlook.com>
2022-11-29 18:18:19 +0700

    4.3.6. Math.abs [#373616 #365665]

--- src/main/java/ru/job4j/ChessBoard.java
+++ src/main/java/ru/job4j/ChessBoard.java
@@ -5,7 +5,7 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 - x2 == y1 - y2) {
+            if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
                 rsl = Math.abs(x2 - x1);
                 rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
             }
@@ -15,5 +15,9 @@ public class ChessBoard {
 
     private static boolean isValid(int coordinate) {
         return coordinate >= 0 && coordinate <= 7;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(way(7, 0, 0, 7));
     }
 }
