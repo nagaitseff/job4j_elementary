147a36038196f393cac876eb42bb15945369dd80
Нагайцев Евгений Петрович <nagaitsev@outlook.com>
2022-12-02 10:59:24 +0700

    4.5. Отладка программы в IDEA [#159464 #366074]

--- src/main/java/ru/job4j/ThreeMax.java
+++ src/main/java/ru/job4j/ThreeMax.java
@@ -2,12 +2,12 @@ package ru.job4j;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = third;
-        if (first > second && first > third) {
-            result = first;
-        }
-        if (second > first && second > third) {
+        int result = first;
+        if (second >= first && second >= third) {
             result = second;
+        }
+        if (third >= first && third >= second) {
+            result = third;
         }
         return result;
     }
